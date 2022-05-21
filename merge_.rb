b594376699fc96e4b8b0632afa30953f38f196e2
Takeshi-Ueki <takeshi.c.0104@gmail.com>
2022-05-21 22:24:20 +0900

    2022/05/21

---
+++ merge_.rb
@@ -0,0 +1,9 @@
+score = {"国語" => 80, "数学" => 75, "英語" => 100}
+
+score2 = {"体育" => 45, "美術" => 95}
+
+score3 = score.merge!(score2)
+
+p score3
+p score2
+p score
---
+++ unshift.rb
@@ -0,0 +1,8 @@
+color =["blue", "red", "yellow"]
+p color
+
+color.unshift("black")
+p color
+
+color.push("green")
+p color
